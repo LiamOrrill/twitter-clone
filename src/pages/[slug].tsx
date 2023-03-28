b1138608c90c57c686a042cb676ad2bb45ba98ff
Liam <liamrumpypumpy@protonmail.com>
2023-03-28 23:48:19 +0100

    chore renderposts

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,25 @@ import { api } from "@/utils/api";
 import { type NextPage } from "next";
 import Head from "next/head";
 
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.post.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0)
+    return <div className="text-xl text-slate-300">No posts found</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map(({ post, author }) => {
+        return <PostView post={post} key={post.id} author={author} />;
+      })}
+    </div>
+  );
+};
+
 const Profile: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
     username,
@@ -26,7 +45,8 @@ const Profile: NextPage<{ username: string }> = ({ username }) => {
         </div>
         <div className="h-[64px]" />
         <div className="p-4 text-2xl">{`@${data.name || ""}`}</div>
-        <div className="w-full border-b border-slate-400"></div>
+        <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
@@ -42,6 +62,8 @@ import { type GetStaticProps } from "next";
 import { appRouter } from "@/server/api/root";
 import { PageLayout } from "@/components/layout";
 import Image from "next/image";
+import { LoadingPage } from "@/components/loading";
+import { PostView } from "@/components/postview";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createProxySSGHelpers({
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -90,4 +90,65 @@ export const postRouter = createTRPCRouter({
 
       return post;
     }),
+
+  getPostsByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const posts = await ctx.prisma.post.findMany({
+        where: {
+          authorId: input.userId,
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          content: true,
+          authorId: true,
+        },
+        take: 100,
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      if (!posts) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Posts not found",
+        });
+      }
+
+      const users = await ctx.prisma.user.findMany({
+        select: {
+          id: true,
+          image: true,
+          name: true,
+        },
+        where: {
+          id: {
+            in: posts.map((post) => post.authorId),
+          },
+        },
+        take: 100,
+      });
+
+      return posts.map((post) => {
+        const author = users.find((user) => user.id === post.authorId);
+
+        if (!author || !author.name || !author.image) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: `Could not find author for post`,
+          });
+        }
+
+        return {
+          post,
+          author: {
+            id: author.id,
+            name: author.name,
+            image: author.image,
+          },
+        };
+      });
+    }),
 });
